5f6eec2f5bf14ad0a198c8cc2d485a7a79e30e35
Neha Sahu <1802.nehasahu@gmail.com>
2023-07-27 16:44:56 +0530

    creating a project with cargo

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// use conmmand: cargo run in the terminal to run the program in the terminal.
+// use command: cargo build to build the program.This command creates an executable file in target/debug/hello_cargo
+// use command: cargo check to check whether the program can be compiled successfully, but it does not generate an executable file.
