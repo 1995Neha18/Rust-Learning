989da939b73be86e18a7f27c6c241caa6c68c392
Neha Sahu <1802.nehasahu@gmail.com>
2023-07-27 17:35:21 +0530

    creating a command line program

---
+++ minigrep/src/main.rs
@@ -0,0 +1,20 @@
+// use std::env;
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//     dbg!(args);
+// }
+
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
