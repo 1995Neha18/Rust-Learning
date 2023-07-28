16081af2a384fb1405e6e00e53a8944824980fe3
Neha Sahu <1802.nehasahu@gmail.com>
2023-07-28 19:15:22 +0530

    applying changes using function and reference

--- mutability/src/main.rs
+++ mutability/src/main.rs
@@ -10,23 +10,37 @@
 
 // Same thing but with strings.
 
-fn main () {
-  let s1 = String::from("hello");
-  // let s2 = s1; (this will throw an error because we are trying to use s1 after we have moved it to s2.)
-  let mut s2 = s1.clone();
-  s2.push_str(", world!");
-  println!("s1 = {}, s2 = {}", s1,s2);
+fn main() {
+    let s1 = String::from("hello");
+    // let s2 = s1; (this will throw an error because we are trying to use s1 after we have moved it to s2.)
+    let mut s2 = s1.clone();
+    s2.push_str(", world!");
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
-// output :- error[E0382]: borrow of moved value: `s1`, 
+// output :- error[E0382]: borrow of moved value: `s1`,
 //this won't compile because we are trying to use s1 after we have moved it to s2.
 // So we need to use clone method to copy the value of s1 to s2.
 // --------------------------------------------------------------------------
 
 // -------------- Using references to solve the problem ----------------
 
-fn main () {
-  let s1 = String::from("hello");
-  let s2 = &s1; // s2 is a reference to s1, so s2 doesn't own the value of s1.
-  println!("s1 = {}, s2 = {}", s1,s2);
-}
+// fn main () {
+//   let s1 = String::from("hello");
+//   let s2 = &s1; // s2 is a reference to s1, so s2 doesn't own the value of s1.
+//   println!("s1 = {}, s2 = {}", s1,s2);
+// }
 // --------------------------------------------------------
+
+// ------------ Function that can change things -------------
+
+fn append_dot(t: &mut String) {
+    t.push_str(".");
+}
+
+fn main() {
+    let mut s = String::from("Hello");
+    append_dot(&mut s);
+    println!("{}", s);
+}
+
+// ----------------------------------------------
