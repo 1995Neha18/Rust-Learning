954c9b309bd476352562288c2d278bc85414c147
Neha Sahu <1802.nehasahu@gmail.com>
2023-07-28 15:46:37 +0530

    calculating square and cube of a number

---
+++ square/src/main.rs
@@ -0,0 +1,19 @@
+// let create a program which calculates square of a number.
+
+fn square(x: i32) -> i32 {
+    x * x
+}
+
+fn main() {
+    println!("The square of {} is {}", 2, square(2));
+}
+
+// ------------------------------------------------
+
+// fn cube (x:i32) -> i32 {
+//  x*x*x
+// }
+
+// fn main() {
+//     println!("The cube of {} is {}", 3 ,  cube(3));
+// }
