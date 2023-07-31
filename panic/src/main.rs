907a3e632db49475d167d11fa6bbf497aa67163e
Neha Sahu <1802.nehasahu@gmail.com>
2023-07-31 18:39:44 +0530

    result enum can also be used instead of panic

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,13 +1,36 @@
-fn drink(beverage: &str) {
-    // You shouldn't drink too much sugary beverages.
-    if beverage == "lemonade" {
-        panic!("AAAaaaaa!!!!");
-    }
+// fn drink(beverage: &str) {
+//     // You shouldn't drink too much sugary beverages.
+//     if beverage == "lemonade" {
+//         panic!("AAAaaaaa!!!!");
+//     }
 
-    println!("Some refreshing {} is all I need.", beverage);
+//     println!("Some refreshing {} is all I need.", beverage);
+// }
+
+// fn main() {
+//     drink("water");
+//     drink("lemonade");
+// }
+
+// --------- same code but using Result instead of panic ----------------
+
+fn drink(beverage: &str) -> Result<(), &'static str> {
+ // You shouldn't drink too much sugary beverages.
+ if beverage == "lemonade" {
+     return Err("AAAaaaaa!!!!");
+ }
+
+ println!("Some refreshing {} is all I need.", beverage);
+ Ok(())
 }
 
 fn main() {
-    drink("water");
-    drink("lemonade");
+ if let Err(error_msg) = drink("water") {
+     println!("Error: {}", error_msg);
+ }
+
+ if let Err(error_msg) = drink("lemonade") {
+     println!("Error: {}", error_msg);
+ }
 }
+
