322359b42a64684ed1cfb24fd889046e80c9e65a
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-01 20:29:29 +0530

    checking the opposite case with false value

--- newadder/src/lib.rs
+++ newadder/src/lib.rs
@@ -67,56 +67,56 @@ mod tests {
 
 //--------------- Adding Custom Failure Messages--------------
 
-// #[derive(Debug)]
-// struct Rectangle {
-//     width: u32,
-//     height: u32,
-// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// --snip--
 // impl Rectangle {
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
+//  fn can_hold(&self, other: &Rectangle) -> bool {
+//      self.width < other.width && self.height > other.height
+//  }
 // }
 
-// // --snip--
-// // impl Rectangle {
-// //  fn can_hold(&self, other: &Rectangle) -> bool {
-// //      self.width < other.width && self.height > other.height
-// //  }
-// // }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+    #[test]
+    fn larger_can_hold_smaller() {
+        // --snip--
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-//     #[test]
-//     fn larger_can_hold_smaller() {
-//         // --snip--
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
+        assert!(larger.can_hold(&smaller));
+    }
 
-//         assert!(larger.can_hold(&smaller));
-//     }
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-//     #[test]
-//     fn smaller_cannot_hold_larger() {
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
-
-//         assert!(!smaller.can_hold(&larger));
-//     }
-// }
+        assert!(!smaller.can_hold(&larger));
+    }
+}
