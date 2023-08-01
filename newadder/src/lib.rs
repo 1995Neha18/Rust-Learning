e646d4aafde081b6fd66dc4bbbac4dc13cc53717
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-01 20:27:49 +0530

    checking result with the assert macro

--- newadder/src/lib.rs
+++ newadder/src/lib.rs
@@ -34,36 +34,36 @@ mod tests {
 
 //--------------- Checking Results with the assert! Macro--------------
 
-// #[derive(Debug)]
-// struct Rectangle {
-//     width: u32,
-//     height: u32,
-// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-// impl Rectangle {
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
-// }
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn larger_can_hold_smaller() {
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-//         assert!(larger.can_hold(&smaller));
-//     }
-// }
+        assert!(larger.can_hold(&smaller));
+    }
+}
 
 //--------------- Adding Custom Failure Messages--------------
 
