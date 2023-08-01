269112b88d0c7603231f71573ec4ba8d9c87d278
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-01 20:20:40 +0530

    usecase of assert!

---
+++ assertEq/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    
+    let my_name = "Neha";
+    assert!(my_name == "Neha");
+    assert_eq!(my_name, "Neha");
+    assert_ne!(my_name, "Sneha");
+
+}
+
+// assert! //  when the value is true
+// assert_eq! // when the two values are equal
+// assert_ne! // when the two values are not equal
