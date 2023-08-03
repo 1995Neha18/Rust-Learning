08a19783b953a345538f2068ee77c51e1c40d81f
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-03 16:55:57 +0530

    how to use method with struct

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -44,16 +44,44 @@
 
 //--------------- Using Tuple Structs Without Named Fields to Create Different Types ---------------//
 
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
+// struct Color(i32, i32, i32);
+// struct Point(i32, i32, i32);
 
-fn main() {
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+// fn main() {
+//     let black = Color(0, 0, 0);
+//     let origin = Point(0, 0, 0);
 
-    // Display color and point information
-    println!("Black Color: {}, {}, {}", black.0, black.1, black.2);
-    println!("Origin Point: {}, {}, {}", origin.0, origin.1, origin.2);
+//     // Display color and point information
+//     println!("Black Color: {}, {}, {}", black.0, black.1, black.2);
+//     println!("Origin Point: {}, {}, {}", origin.0, origin.1, origin.2);
+// }
+
+
+// ------------------------- Using the impl Keyword to Define Methods on a Struct -------------------------//
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
+// first parameter is always self, which represents the instance of the struct the method is being called on.
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
 
