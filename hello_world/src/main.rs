4c7650e092b9f7c0aea3fe91d75fd5bf39e33c62
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-08 00:01:47 +0530

    using num_trait for numeric values

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,15 @@
+use num_traits::Float; // 0.2.1
+
 fn main() {
-    println!("Hello, world!");
+    let f1: f32 = 2.0;
+    let f2: f64 = 3.0;
+    let i1: i32 = 3;
+
+    println!("{:?}", sqrt(f1));
+    println!("{:?}", sqrt(f2));
+    println!("{:?}", sqrt(i1)); // error: the trait bound `{integer}: Float` is not satisfied
+}
+
+fn sqrt<T: Float>(input: T) -> T {
+    input.sqrt()
 }
