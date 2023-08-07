b5dc26d84b03aa4d8895a3d327ba635c73fe74a3
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-07 22:52:27 +0530

    Parsing JSON in rust using serde and reqwest

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,7 +1,37 @@
-fn main() {
-    println!("Hello, world!");
-}
+// fn main() {
+//     println!("Hello, world!");
+// }
 
 // use conmmand: cargo run in the terminal to run the program in the terminal.
-// use command: cargo build to build the program.This command creates an executable file in target/debug/hello_cargo
-// use command: cargo check to check whether the program can be compiled successfully, but it does not generate an executable file.
+// use command: cargo build to build the program.This command creates an executable file
+// in target/debug/hello_cargo
+// use command: cargo check to check whether the program can be compiled successfully,
+//but it does not generate an executable file.
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    id: Option<i32>,
+    title: String,
+    completed: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // Receive type-checked JSON
+    
+    let todos: Vec<Todo> = reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos?userId=1")
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", todos);
+
+    println!("{:#?}", todos);
+    Ok(())
+}
