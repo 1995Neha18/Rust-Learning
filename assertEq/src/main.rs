1d9a24dcb45474e04973133ff6e7bf521d623411
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-07 16:39:22 +0530

    learned how to create and run file in src directory other than main.rs file

--- assertEq/src/main.rs
+++ assertEq/src/main.rs
@@ -1,11 +1,18 @@
+mod neha;
+
 fn main() {
-    
+    neha::hello();
     let my_name = "Neha";
     assert!(my_name == "Neha");
     assert_eq!(my_name, "Neha");
-    assert_ne!(my_name, "Sneha");
+    assert_ne!(my_name, "Neha");
+
 
 }
+
+
+
+
 
 // assert! //  when the value is true
 // assert_eq! // when the two values are equal
