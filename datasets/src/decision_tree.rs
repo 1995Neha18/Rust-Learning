1eb3c13b7506fc020972f046f1180af513a994d3
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-08 07:36:19 +0530

    decision tree implementation

---
+++ datasets/src/decision_tree.rs
@@ -0,0 +1,13 @@
+use linfa_trees::DecisionTree;
+use linfa::prelude::*;
+
+pub fn run_decision_tree() {
+    let (train, test) = linfa_datasets::iris().split_with_ratio(0.9);
+
+    let model = DecisionTree::params().fit(&train).unwrap();
+
+    let predictions = model.predict(&test);
+
+    println!("{:?}", predictions);
+    println!("{:?}", test.targets);
+}
