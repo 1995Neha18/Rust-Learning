42f45f68d151ebb97019c6ef7c1b6b0274815148
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-08 08:29:18 +0530

    another example of random num

--- datasets/src/random.rs
+++ datasets/src/random.rs
@@ -9,16 +9,38 @@
 // }
 // }
 
+// --------------------------------------------------
+
+// use rand::Rng;
+
+// pub fn random_num() {
+//     let mut rng = rand::thread_rng();
+//     let x: u8 = rng.gen();
+//     println!("{}", x);
+
+//     if rng.gen::<bool>() {
+//         println!("Heads!");
+//     } else {
+//         println!("Tails!");
+//     }
+// }
+
+// --------------------------------------
+
 use rand::Rng;
 
 pub fn random_num() {
-    let mut rng = rand::thread_rng();
-    let x: u8 = rng.gen();
-    println!("{}", x);
 
-    if rng.gen::<bool>() {
-        println!("Heads!");
-    } else {
-        println!("Tails!");
-    }
+    let mut rng = rand::thread_rng();
+    //This line creates a mutable instance of a random number generator using rand::thread_rng(). 
+    //This generator is specific to the current thread and can be used to generate random numbers.
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen(); 
+
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
 }
