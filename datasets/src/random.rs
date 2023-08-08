a296c42c61f7c9a60c4a54527654f22cc9cd000e
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-08 08:11:18 +0530

    generating random num using rand::Rng

--- datasets/src/random.rs
+++ datasets/src/random.rs
@@ -1,10 +1,24 @@
-use rand::prelude::*;
+// use rand::prelude::*;
+
+// pub fn random_num() {
+//  let x: u8 = random();
+//  println!("{}", x);
+
+//  if random() { // generates a boolean
+//      println!("Heads!");
+// }
+// }
+
+use rand::Rng;
 
 pub fn random_num() {
- let x: u8 = random();
- println!("{}", x);
+    let mut rng = rand::thread_rng();
+    let x: u8 = rng.gen();
+    println!("{}", x);
 
- if random() { // generates a boolean
-     println!("Heads!");
-}
+    if rng.gen::<bool>() {
+        println!("Heads!");
+    } else {
+        println!("Tails!");
+    }
 }
