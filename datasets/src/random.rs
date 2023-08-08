57cb039f758373ae2df74f78ece46ce465d48898
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-08 08:40:41 +0530

    random num within range

--- datasets/src/random.rs
+++ datasets/src/random.rs
@@ -27,20 +27,31 @@
 
 // --------------------------------------
 
-use rand::Rng;
+// use rand::Rng;
+
+// pub fn random_num() {
+
+//     let mut rng = rand::thread_rng();
+//     //This line creates a mutable instance of a random number generator using rand::thread_rng(). 
+//     //This generator is specific to the current thread and can be used to generate random numbers.
+
+//     let n1: u8 = rng.gen();
+//     let n2: u16 = rng.gen(); 
+
+//     println!("Random u8: {}", n1);
+//     println!("Random u16: {}", n2);
+//     println!("Random u32: {}", rng.gen::<u32>());
+//     println!("Random i32: {}", rng.gen::<i32>());
+//     println!("Random float: {}", rng.gen::<f64>());
+// }
+
+// ------------ generating random number within a range -------------------
+
+use rand::Rng; 
 
 pub fn random_num() {
-
-    let mut rng = rand::thread_rng();
-    //This line creates a mutable instance of a random number generator using rand::thread_rng(). 
-    //This generator is specific to the current thread and can be used to generate random numbers.
-
-    let n1: u8 = rng.gen();
-    let n2: u16 = rng.gen(); 
-
-    println!("Random u8: {}", n1);
-    println!("Random u16: {}", n2);
-    println!("Random u32: {}", rng.gen::<u32>());
-    println!("Random i32: {}", rng.gen::<i32>());
-    println!("Random float: {}", rng.gen::<f64>());
+    // Generate random number in the range [0, 99]
+    let num = rand::thread_rng().gen_range(0..100);
+    println!("{}", num);
 }
+
