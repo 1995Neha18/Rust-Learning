2a5da7b72449c264b18ca350de45e2e2cc1f8d51
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-09 20:48:41 +0530

    iterator usage

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -14,23 +14,34 @@
 
 // --------- same code but using Result instead of panic ----------------
 
-fn drink(beverage: &str) -> Result<(), &'static str> {
- // You shouldn't drink too much sugary beverages.
- if beverage == "lemonade" {
-     return Err("AAAaaaaa!!!!");
- }
+// fn drink(beverage: &str) -> Result<(), &'static str> {
+//  // You shouldn't drink too much sugary beverages.
+//  if beverage == "lemonade" {
+//      return Err("AAAaaaaa!!!!");
+//  }
 
- println!("Some refreshing {} is all I need.", beverage);
- Ok(())
-}
+//  println!("Some refreshing {} is all I need.", beverage);
+//  Ok(())
+// }
 
-fn main() {
- if let Err(error_msg) = drink("water") {
-     println!("Error: {}", error_msg);
- }
+// fn main() {
+//  if let Err(error_msg) = drink("water") {
+//      println!("Error: {}", error_msg);
+//  }
 
- if let Err(error_msg) = drink("lemonade") {
-     println!("Error: {}", error_msg);
+//  if let Err(error_msg) = drink("lemonade") {
+//      println!("Error: {}", error_msg);
+//  }
+// }
+
+// ----------------- Iterator -----------------
+
+ fn main () {
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v1_iter = v1.iter(); // v1_iter is immutable
+
+    for value in v1_iter {
+        println!("Got: {}", value);
+    }
  }
-}
 
