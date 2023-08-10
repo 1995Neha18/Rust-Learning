c89fbfec5a9960440f5c33abf9a8d3038c6afea3
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-10 08:27:02 +0530

    another comparison example

--- my_cmp/src/main.rs
+++ my_cmp/src/main.rs
@@ -1,17 +1,41 @@
-mod comparison; // Import the comparison module from comparison.rs
+// mod comparison; // Import the comparison module from comparison.rs
 
-use std::cmp::Ordering; // Importing the Ordering enum from the standard library
-use comparison::compare_numbers; // Importing the compare_numbers function
+// use std::cmp::Ordering; // Importing the Ordering enum from the standard library
+// use comparison::compare_numbers; // Importing the compare_numbers function
+
+// fn main() {
+//     let number1 = 5;
+//     let number2 = 10;
+
+//     let comparison = compare_numbers(number1, number2);
+
+//     match comparison {
+//         Ordering::Less => println!("Number 1 is less than Number 2"),
+//         Ordering::Equal => println!("Number 1 is equal to Number 2"),
+//         Ordering::Greater => println!("Number 1 is greater than Number 2"),
+//     }
+// }
+
+
+use std::cmp::Ordering;
 
 fn main() {
-    let number1 = 5;
-    let number2 = 10;
+    let mut strings = vec![
+     "banana".to_string(),
+     "cherry".to_string(),
+     "grape".to_string(),
+     "apple".to_string(),
+        "date".to_string(),
+    ];
 
-    let comparison = compare_numbers(number1, number2);
+    // Sort the vector of strings using custom comparison logic
+    strings.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
 
-    match comparison {
-        Ordering::Less => println!("Number 1 is less than Number 2"),
-        Ordering::Equal => println!("Number 1 is equal to Number 2"),
-        Ordering::Greater => println!("Number 1 is greater than Number 2"),
+    // Print the sorted strings
+    for s in &strings {
+        println!("{}", s);
     }
 }
+
+
+
