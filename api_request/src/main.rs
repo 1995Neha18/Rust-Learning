b3e74c91c261ec7601ac9dedbf21ca831f5a6749
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-12 16:46:52 +0530

    HTTP delete request

--- api_request/src/main.rs
+++ api_request/src/main.rs
@@ -1,27 +1,12 @@
 use reqwest::Client;
-use serde::{ Deserialize, Serialize };
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Todo {
-    #[serde(rename = "userId")]
-    user_id: i32,
-    id: Option<i32>,
-    title: String,
-    completed: bool,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let updated_fields = serde_json::json!({
-    "title": "Hello Neha!".to_owned()
-});
+    let response = Client::new()
+        .delete("https://jsonplaceholder.typicode.com/todos/1") // Assuming you want to delete todo with ID 1
+        .send()
+        .await?;
 
-    let updated_todo: Todo = Client::new()
-        .patch("https://jsonplaceholder.typicode.com/todos/1")
-        .json(&updated_fields)
-        .send().await?
-        .json().await?;
-
-    println!("{:#?}", updated_todo);
+    println!("Response: {:#?}", response);
     Ok(())
 }
