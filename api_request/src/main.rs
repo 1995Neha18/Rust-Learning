15dbf11b58a5778732282ea5444a84fbb01fd4a6
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-13 23:31:25 +0530

    chrono crate for day,date & time

--- api_request/src/main.rs
+++ api_request/src/main.rs
@@ -84,63 +84,74 @@
 
 // ----------------------------------------------------------------------------
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut hashmap = HashMap::new();
+    
+//     // insert() method
+//     hashmap.insert("key1", "value1");
+//     hashmap.insert("key2", "value2");
+    
+//     // get() method
+//     if let Some(value) = hashmap.get("key1") {
+//         println!("Value for key 'key1': {}", value);
+//     }
+    
+//     // remove() method
+//     if let Some(removed_value) = hashmap.remove("key2") {
+//         println!("Removed value for key 'key2': {}", removed_value);
+//     }
+    
+//     // contains_key() method
+//     if hashmap.contains_key("key1") {
+//         println!("The map contains 'key1'.");
+//     } else {
+//         println!("The map does not contain 'key1'.");
+//     }
+    
+//     // len() method
+//     println!("Number of key-value pairs in the map: {}", hashmap.len());
+    
+//     // is_empty() method
+//     if hashmap.is_empty() {
+//         println!("The map is empty.");
+//     } else {
+//         println!("The map is not empty.");
+//     }
+    
+//     // keys() method
+//     for key in hashmap.keys() {
+//         println!("Key: {}", key);
+//     }
+    
+//     // values() method
+//     for value in hashmap.values() {
+//         println!("Value: {}", value);
+//     }
+    
+//     // iter() method
+//     for (key, value) in hashmap.iter() {
+//         println!("Key: {}, Value: {}", key, value);
+//     }
+    
+//     // entry() method
+//     let entry = hashmap.entry("key3").or_insert("default");
+//     println!("Value for key 'key3': {}", entry);
+    
+//     // clear() method
+//     hashmap.clear();
+//     println!("After clearing the map, is it empty? {}", hashmap.is_empty());
+// }
+
+// --------------------------------------------------------
+
+use chrono::prelude::*;
 
 fn main() {
-    let mut hashmap = HashMap::new();
+    let utc: DateTime<Utc> = Utc::now();
+    let local: DateTime<Local> = Local::now();
     
-    // insert() method
-    hashmap.insert("key1", "value1");
-    hashmap.insert("key2", "value2");
-    
-    // get() method
-    if let Some(value) = hashmap.get("key1") {
-        println!("Value for key 'key1': {}", value);
-    }
-    
-    // remove() method
-    if let Some(removed_value) = hashmap.remove("key2") {
-        println!("Removed value for key 'key2': {}", removed_value);
-    }
-    
-    // contains_key() method
-    if hashmap.contains_key("key1") {
-        println!("The map contains 'key1'.");
-    } else {
-        println!("The map does not contain 'key1'.");
-    }
-    
-    // len() method
-    println!("Number of key-value pairs in the map: {}", hashmap.len());
-    
-    // is_empty() method
-    if hashmap.is_empty() {
-        println!("The map is empty.");
-    } else {
-        println!("The map is not empty.");
-    }
-    
-    // keys() method
-    for key in hashmap.keys() {
-        println!("Key: {}", key);
-    }
-    
-    // values() method
-    for value in hashmap.values() {
-        println!("Value: {}", value);
-    }
-    
-    // iter() method
-    for (key, value) in hashmap.iter() {
-        println!("Key: {}, Value: {}", key, value);
-    }
-    
-    // entry() method
-    let entry = hashmap.entry("key3").or_insert("default");
-    println!("Value for key 'key3': {}", entry);
-    
-    // clear() method
-    hashmap.clear();
-    println!("After clearing the map, is it empty? {}", hashmap.is_empty());
+    println!("UTC now is: {}", utc);
+    println!("Local now is: {}", local);
 }
-
