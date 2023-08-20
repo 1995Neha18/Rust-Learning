2f34ef58ee5d2953aff48e865050af9a7f27f62b
Neha Sahu <1802.nehasahu@gmail.com>
2023-08-20 22:19:40 +0530

    subscribe to events using the Observer

--- usb_device/src/main.rs
+++ usb_device/src/main.rs
@@ -1,13 +1,43 @@
-fn main() {
- for device in rusb::devices().unwrap().iter() {
-     let device_desc = device.device_descriptor().unwrap();
+// fn main() {
+//  for device in rusb::devices().unwrap().iter() {
+//      let device_desc = device.device_descriptor().unwrap();
 
-     println!(
-         "Bus {:03} Device {:03} ID {:04x}:{:04x}",
-         device.bus_number(),
-         device.address(),
-         device_desc.vendor_id(),
-         device_desc.product_id()
-     );
- }
+//      println!(
+//          "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+//          device.bus_number(),
+//          device.address(),
+//          device_desc.vendor_id(),
+//          device_desc.product_id()
+//      );
+//  }
+// }
+
+// -------------------------------------------------
+
+// fn main() {
+//     let devices = usb_enumeration::enumerate(None, None);
+
+//     println!("{:#?}", devices);
+// }
+
+
+fn main(){
+ use usb_enumeration::{Observer, Event};
+
+let sub = Observer::new()
+    .with_poll_interval(2)
+    .with_vendor_id(6127)
+    .with_product_id(24785)
+    .subscribe();
+
+// when sub is dropped, the background thread will close
+
+for event in sub.rx_event.iter() {
+    match event {
+        Event::Initial(d) => println!("Initial devices: {:?}", d),
+        Event::Connect(d) => println!("Connected device: {:?}", d),
+        Event::Disconnect(d) => println!("Disconnected device: {:?}", d),
+    }
 }
+}
+
