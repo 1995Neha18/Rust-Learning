4008420dd0393d6b447762a1b3e7b1ddc1a56707
Neha Sahu <1802.nehasahu@gmail.com>
2023-09-18 20:21:37 +0530

    usecase of setting env variables in rust

--- imports_usage/src/main.rs
+++ imports_usage/src/main.rs
@@ -1,11 +1,28 @@
-use std::env;
+// ---------------- Example-02: Reading Environment Variables ------------------
+// use std::env;
 
-fn main() {
-    if let Ok(user) = env::var("USERNAME") {
-       println!("User: {}", user)
+// fn main() {
+//     if let Ok(user) = env::var("USERNAME") {
+//        println!("User: {}", user)
        
+//     }
+//     else {
+//       println!("USERNAME environment variable not found")
+//     }
+// }
+
+
+// --------------------- Example-02: Setting Environment Variables --------------------
+
+use std::env;
+ fn main(){
+    // set a custom variable.
+    env::set_var("MY VARIABLE", "Hello, Rust!");
+
+    if let Ok(value) = env::var("MY VARIABLE"){
+       println!("MY VARIABLE: {}", value)
     }
     else {
-      println!("USERNAME environment variable not found")
+      println!("MY VARIABLE environment variable not found")
     }
-}
+ }
