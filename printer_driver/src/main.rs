05a199774b9fb20c10a8cf424ff12e8b29d8d78a
Neha Sahu <1802.nehasahu@gmail.com>
2023-09-22 21:01:38 +0530

    extracting printer name list

--- printer_driver/src/main.rs
+++ printer_driver/src/main.rs
@@ -1,24 +1,29 @@
-use std::str;
 use std::process::Command;
+use std::str;
 
 fn main() {
-    let output = Command::new("wmic")
-        .arg("printer get name")
+    let output = Command::new(r"C:\Windows\System32\wbem\WMIC.exe")
+        .arg("printer")
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 
     if output.status.success() {
         let output_str = str::from_utf8(&output.stdout).expect("Invalid UTF-8");
+
         let lines: Vec<&str> = output_str.lines().collect();
+
+        if let Some(header) = lines.first() {
+            println!("{}", header);
+        }
 
         for line in lines.iter().skip(1) {
             let printer_name = line.trim();
             if !printer_name.is_empty() {
-                println!("Printer Name: {}", printer_name);
+                println!("{}", printer_name);
             }
         }
     } else {
         let error_str = str::from_utf8(&output.stderr).expect("Invalid UTF-8");
-        eprintln!("Error: {}", error_str);
+        eprintln!("Error executing WMIC: {}", error_str);
     }
 }
