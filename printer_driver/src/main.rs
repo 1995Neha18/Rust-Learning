786d1b96e9123a5e73018d45f34ab8aeeb3adf54
Neha Sahu <1802.nehasahu@gmail.com>
2023-09-22 21:08:30 +0530

    extracting printer list with powerhell

--- printer_driver/src/main.rs
+++ printer_driver/src/main.rs
@@ -1,29 +1,54 @@
+// -------------------------- Method 1 -------------------------
+
+// use std::process::Command;
+// use std::str;
+
+// fn main() {
+//     let output = Command::new(r"C:\Windows\System32\wbem\WMIC.exe")
+//         .arg("printer")
+//         .output()
+//         .expect("Failed to execute process");
+
+//     if output.status.success() {
+//         let output_str = str::from_utf8(&output.stdout).expect("Invalid UTF-8");
+
+//         let lines: Vec<&str> = output_str.lines().collect();
+
+//         if let Some(header) = lines.first() {
+//             println!("{}", header);
+//         }
+
+//         for line in lines.iter().skip(1) {
+//             let printer_name = line.trim();
+//             if !printer_name.is_empty() {
+//                 println!("{}", printer_name);
+//             }
+//         }
+//     } else {
+//         let error_str = str::from_utf8(&output.stderr).expect("Invalid UTF-8");
+//         eprintln!("Error executing WMIC: {}", error_str);
+//     }
+// }
+// -------------------- x ------------------ x ------------------
+
+// -------------------------- Method 2 --------------------------
+
 use std::process::Command;
-use std::str;
 
 fn main() {
-    let output = Command::new(r"C:\Windows\System32\wbem\WMIC.exe")
-        .arg("printer")
+    let output = Command::new("powershell")
+        .arg("-Command")
+        .arg(
+            "Get-WmiObject -Query 'SELECT * FROM Win32_Printer' | Select-Object -ExpandProperty Name"
+        )
         .output()
         .expect("Failed to execute process");
 
     if output.status.success() {
-        let output_str = str::from_utf8(&output.stdout).expect("Invalid UTF-8");
-
-        let lines: Vec<&str> = output_str.lines().collect();
-
-        if let Some(header) = lines.first() {
-            println!("{}", header);
-        }
-
-        for line in lines.iter().skip(1) {
-            let printer_name = line.trim();
-            if !printer_name.is_empty() {
-                println!("{}", printer_name);
-            }
-        }
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        println!("{}", output_str);
     } else {
-        let error_str = str::from_utf8(&output.stderr).expect("Invalid UTF-8");
-        eprintln!("Error executing WMIC: {}", error_str);
+        let error_str = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error executing PowerShell: {}", error_str);
     }
 }
